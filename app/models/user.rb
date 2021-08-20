045ce2cf9623a4a0e69dbe064422b9568d18c8a6
Christine Jeon <csjeon28@gmail.com>
2021-08-20 14:49:08 -0400

    added password requirements and other validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
     has_many :workouts
     has_many :exercises
+
+    has_secure_password
+
+    PASSWORD_REQ = /\A
+        (?=.{8,}) # Minimum 8 characters
+        (?=.*\d) # Contains at least one number
+        (?=.*[a-z]) # Contains at least one lowercase letter
+        (?=.*[A-Z]) # Contains at least one uppercase letter
+        (?=.*[[:^alnum:]]) # Contains at least one symbol
+    /x
+
+    validates :username, presence: true, uniqueness: true
+    validates :username, length: {minimum: 5}
+    validates :password, format: PASSWORD_REQ
+    validates :password_confirmation, presence: true
 end
