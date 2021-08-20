4c27e935389050c53dda84106b2072a4c4ecf99d
Christine Jeon <csjeon28@gmail.com>
2021-08-20 14:48:27 -0400

    added sessions and users controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    # POST /login (sessions#create)
+    def create
+        user = User.find_by(username: params[:username])
+        if user &.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: {user: user}, status: :ok
+        else
+            render json: {errors: ["Invalid username or password"]}, status: :unauthorized
+        end
+    end
+
+    # DELETE /logout (sessions#destroy)
+    def destroy
+        if session[:user_id]
+            session.delete :user_id
+            head :no_content
+        else
+            render json: {errors: ["You are not logged in"]}, status: :unauthorized
+        end
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
-end
+    skip_before_action :authorize, only: :create
+  
+    # POST /users (users#create)
+    def create
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+    end
+  
+    # GET /users/:id (users#show)
+    def show
+      render json: @current_user
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:username, :password, :password_confirmation)
+    end
+  
+  end
