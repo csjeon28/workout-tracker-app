30c439acf3fd5d48ea49ba1a898be4b59f4e2c6c
Christine Jeon <csjeon28@gmail.com>
2021-08-20 19:08:53 -0400

    added styled components

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -32,30 +32,30 @@ const NavBar = ({ user, setUser }) => {
 }
 
 const Wrapper = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 8px;
-`;
+  display: flex
+  justify-content: center
+  align-items: center
+  padding: 8px
+`
 
 const Logo = styled.h1`
-  font-family: "Permanent Marker", cursive;
-  font-size: 3rem;
-  color: deeppink;
-  margin: 0;
-  line-height: 1;
+  font-family: "Permanent Marker", cursive
+  font-size: 3rem
+  color: deeppink
+  margin: 0
+  line-height: 1
 
   a {
-    color: inherit;
-    text-decoration: none;
+    color: inherit
+    text-decoration: none
   }
-`;
+`
 
 const Nav = styled.nav`
-  display: flex;
-  gap: 4px;
-  position: absolute;
-  right: 8px;
-`;
+  display: flex
+  gap: 4px
+  position: absolute
+  right: 8px
+`
 
 export default NavBar
