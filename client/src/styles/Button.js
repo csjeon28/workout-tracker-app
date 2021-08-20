605847d4c8e876defb91a8550f7d1d76e4b1606d
Christine Jeon <csjeon28@gmail.com>
2021-08-20 19:24:46 -0400

    added button features

--- client/src/styles/Button.js
+++ client/src/styles/Button.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const COLORS = {
   primary: {
@@ -50,4 +50,4 @@ const OutlineButton = styled(ButtonBase)`
   }
 `;
 
-export default Button;
+export default Button
