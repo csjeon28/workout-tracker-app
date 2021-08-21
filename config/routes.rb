5f9136d1591dee14813df2fb2dd21cec2a880005
Christine Jeon <csjeon28@gmail.com>
2021-08-21 03:40:08 -0400

    added create to user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
   
-  post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
 
   post '/login', to: 'sessions#create'
