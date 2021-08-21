f380fa4c6f1a128ae8710c4f15cf5fed92c4e14a
Christine Jeon <csjeon28@gmail.com>
2021-08-21 03:49:22 -0400

    added functionality for login, but need to work on button functionality

---
+++ client/src/components/Login.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import { MainContainer, Input, InputForm, HorizontalLine, LoginButton, SignupButton } from '../styles'
+// import LoginForm from './LoginForm'
+// import SignupForm from './SignupForm'
+import { useState } from 'react'
+// import Errors from './Errors'
+
+const Login = ({ errors, handleUserLoginAndSignup }) => {
+    // const [showLogin, setShowLogin] = useState(true)
+    const [state, setState] = useState({})
+
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        let config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(state)
+        }
+
+        fetch('/login', config)
+            .then(res => res.json())
+            .then(data => handleUserLoginAndSignup(data))
+    }
+
+    return (
+        <MainContainer>
+            <LogoText>Workout Tracker</LogoText>
+            <InputForm onSubmit={handleSubmit}>
+                <Input onChange={handleChange} type='text' placeholder='Username' />
+                <Input onChange={handleChange} type='password' placeholder='Password' />
+                <LoginButton type='submit' content='Log In'></LoginButton>
+            </InputForm>
+            {/* <ButtonContainer>
+            </ButtonContainer> */}
+            <HorizontalLine />
+            {/* <SignupForm onLogin={onLogin} /> */}
+            <SignUpText>New User?</SignUpText>
+            <SignupButton content='Sign Up'>Sign Up</SignupButton>
+        </MainContainer>
+    )
+}
+
+const LogoText = styled.h2`
+  margin: 3rem 0 2rem 0;
+`;
+
+const SignUpText = styled.h4`
+  margin: 2rem 0 2rem 0;
+`;
+
+export default Login
