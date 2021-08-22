add638b03bc68fdc718a2df8e5f71af374bd3c6c
Christine Jeon <csjeon28@gmail.com>
2021-08-21 23:36:14 -0400

    commented out for now until i get functionality working first

--- client/src/styles/LoginButton.js
+++ client/src/styles/LoginButton.js
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-function LoginButton({ content }) {
-    return <LoginButtonStyle>{content}</LoginButtonStyle>
-}
+// function LoginButton({ content }) {
+//     return <LoginButtonStyle>{content}</LoginButtonStyle>
+// }
 
-const LoginButtonStyle = styled.button`
-  background: linear-gradient(to right, #14163c 0%, #062c9e 70%);
-  text-transform: uppercase;
-  letter-spacing: 0.2rem;
-  width: 55%;
-  height: 3rem;
-  border: none;
-  border-radius: 2rem;
-  cursor: pointer;
-  color: white;
-`;
+// const LoginButtonStyle = styled.button`
+//   background: linear-gradient(to right, #14163c 0%, #062c9e 70%);
+//   text-transform: uppercase;
+//   letter-spacing: 0.2rem;
+//   width: 55%;
+//   height: 3rem;
+//   border: none;
+//   border-radius: 2rem;
+//   cursor: pointer;
+//   color: white;
+// `;
 
-export default LoginButton
+// export default LoginButton
