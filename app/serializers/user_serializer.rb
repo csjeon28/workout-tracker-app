2217b234123822519375566ebb7df0900879f063
Christine Jeon <csjeon28@gmail.com>
2021-08-24 02:23:40 -0400

    added exercise serializer and instance methods to display nested json data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,13 @@
-class UserSerializer
-  include JSONAPI::Serializer
-  attributes :id, :username
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :username, :workouts
+
+  def workouts
+    self.object.workouts.map do |workout_obj|
+      {
+        id: workout_obj.id,
+        date: workout_obj.date,
+        weight: workout_obj.weight
+      }
+    end
+  end
 end
--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,5 +1,15 @@
-class WorkoutSerializer
-  include JSONAPI::Serializer
-  attributes :date, :weight
-  belongs_to :user, serializer: :user
+class WorkoutSerializer < ActiveModel::Serializer
+  attributes :id, :date, :weight, :exercises
+  # belongs_to :user, serializer: :user
+
+  def exercises
+    self.object.exercises.map do |exercise_obj|
+      {
+        id: exercise_obj.id,
+        name: exercise_obj.name,
+        calories: exercise_obj.calories,
+        duration: exercise_obj.duration
+      }
+    end
+  end
 end
