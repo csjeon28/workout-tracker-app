9a5de17a98f2d3b4899ef37e2d7cec0acae2193a
Christine Jeon <csjeon28@gmail.com>
2021-08-24 02:21:43 -0400

    commented out code to allow my other app folder to work to test it out

--- client/src/components/CreateWorkout.js
+++ client/src/components/CreateWorkout.js
@@ -1,47 +1,47 @@
-import { useState } from 'react'
-import Errors from './Errors'
+// import { useState } from 'react'
+// import Errors from './Errors'
 
-const CreateWorkout = ({ handleCreateWorkout, errors }) => {
+// const CreateWorkout = ({ handleCreateWorkout, errors }) => {
 
-    const [state, setState] = useState({})
+//     const [state, setState] = useState({})
 
-    const onChange = (e) => {
-        const { name, value } = e.target
-        setState({ ...state, [name]: value })
-    }
+//     const onChange = (e) => {
+//         const { name, value } = e.target
+//         setState({ ...state, [name]: value })
+//     }
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+//     const onSubmit = (e) => {
+//         e.preventDefault()
 
-        const config = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(state)
-        }
+//         const config = {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             },
+//             body: JSON.stringify(state)
+//         }
 
-        fetch('/workouts', config)
-            .then(resp => resp.json())
-            // .then(data => console.log(data))
-            .then(data => handleCreateWorkout(data))
-    }
+//         fetch('/workouts', config)
+//             .then(resp => resp.json())
+//             // .then(data => console.log(data))
+//             .then(data => handleCreateWorkout(data))
+//     }
 
-    return (
-        <div>
-            <Errors errors={errors} />
-            <form onSubmit={onSubmit}>
-                <label>Date:</label>
-                <input onChange={onChange} name='date' type='text' />
-                <br />
-                <label>Weight:</label>
-                <input onChange={onChange} name='weight' type='text' />
-                <br />
-                <input type='submit' value='Create Workout' />
-            </form>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <Errors errors={errors} />
+//             <form onSubmit={onSubmit}>
+//                 <label>Date:</label>
+//                 <input onChange={onChange} name='date' type='text' />
+//                 <br />
+//                 <label>Weight:</label>
+//                 <input onChange={onChange} name='weight' type='text' />
+//                 <br />
+//                 <input type='submit' value='Create Workout' />
+//             </form>
+//         </div>
+//     )
+// }
 
-export default CreateWorkout
+// export default CreateWorkout
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
-import App from './App'
+// import App from './App'
+// just for now until i get this app to function correctly
+import App from '../src/app/App'
 
 ReactDOM.render(
   <Router>
