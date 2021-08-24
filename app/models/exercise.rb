b0bd743445137a9cb2b7912d9e3242719076dcb5
Christine Jeon <csjeon28@gmail.com>
2021-08-24 02:23:03 -0400

    added validations for exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,8 @@
 class Exercise < ApplicationRecord
     belongs_to :user
     belongs_to :workout
+
+    validates :name, presence: true
+    validates :calories, presence: true, numericality: {only_integer: true}
+    validates :duration, presence: true, numericality: {only_integer: true}
 end
