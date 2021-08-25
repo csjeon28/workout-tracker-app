4917f2d0e774d200e6cd6e6b18f26a4eee80e73a
Christine Jeon <csjeon28@gmail.com>
2021-08-25 14:53:29 -0400

    took out clogout function and added a new logout component

--- client/src/app/components/NavBar.js
+++ client/src/app/components/NavBar.js
@@ -1,24 +1,13 @@
-import React from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-const NavBar = ({ currentUser, setCurrentUser }) => {
-    const history = useHistory()
-
-    const handleLogoutClick = () => {
-        let config = {
-            method: 'DELETE'
-        }
-        fetch('/logout', config)
-        setCurrentUser(null)
-        history.push('/login')
-    }
+const NavBar = ({ currentUser }) => {
 
     return (
         <nav>
-            {currentUser ? `${currentUser} is logged in.` : null}
-            <NavLink to='/userhomepage' activeClassName='active-style'>User Home</NavLink>
+            <h2>{currentUser ? `Signed in as: ${currentUser.username}` : null}</h2>
+            <br />
             <NavLink to='/workouts/new' activeClassName='active-style'>Create Workout</NavLink>
-            <NavLink to='/logout' onClick={handleLogoutClick} activeClassName='active-style'>Log Out</NavLink>
+            <NavLink to='/logout' activeClassName='active-style'>Log Out</NavLink>
         </nav>
     )
 }
