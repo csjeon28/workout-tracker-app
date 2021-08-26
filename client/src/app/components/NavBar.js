867ebce8ddf95e0241284034ccd955e5273a59f5
Christine Jeon <csjeon28@gmail.com>
2021-08-26 03:02:55 -0400

    took out link to create workout and added option to switch user

--- client/src/app/components/NavBar.js
+++ client/src/app/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = ({ currentUser }) => {
         <nav>
             <h2>{currentUser ? `Signed in as: ${currentUser.username}` : null}</h2>
             <br />
-            <NavLink to='/workouts/new' activeClassName='active-style'>Create Workout</NavLink>
+            <NavLink to='/login' activeClassName='active-style'>Switch User?</NavLink>
             <NavLink to='/logout' activeClassName='active-style'>Log Out</NavLink>
         </nav>
     )
