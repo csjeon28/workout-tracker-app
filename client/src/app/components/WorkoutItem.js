04cf555c5a1ba3b3958927729ea3f5cc4102b041
Christine Jeon <csjeon28@gmail.com>
2021-08-26 03:03:45 -0400

    delete functionality working

--- client/src/app/components/WorkoutItem.js
+++ client/src/app/components/WorkoutItem.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const WorkoutItem = ({ workout, onDeleteWorkout }) => {
+const WorkoutItem = ({ workout, onDeleteWorkout, editWorkout, editing }) => {
 
     const performDeleteWorkout = () => {
         fetch(`/workouts/${workout.id}`, {
@@ -14,17 +13,53 @@ const WorkoutItem = ({ workout, onDeleteWorkout }) => {
     }
 
     return (
-        <div>
-            <h3>{workout.date}</h3>
-            <div className='workout-item-buttons'>
-                <button onClick={performDeleteWorkout}>Delete</button>
-                <Link to={`/workouts/${workout.id}/edit`}>Edit</Link>
-            </div>
-            <div className='workout-item-weight'>
-                <h4>{workout.weight}</h4>
-            </div>
+        <div key={workout.id}>
+            <h3>Date:{workout.date}</h3>
+            <h4>Weight:{workout.weight}</h4>
+
+            {editing ? (null
+            ) : (
+                <button
+                    onClick={() => { editWorkout(workout) }}
+                >Edit</button>
+            )}
+
+            <button onClick={performDeleteWorkout}>Remove</button>
         </div>
     )
 }
 
 export default WorkoutItem
+
+// import { Link } from 'react-router-dom'
+// // import EditWorkout from './EditWorkout'
+
+// const WorkoutItem = ({ workout, onDeleteWorkout }) => {
+
+    // const performDeleteWorkout = () => {
+    //     fetch(`/workouts/${workout.id}`, {
+    //         method: 'DELETE'
+    //     })
+    //         .then(() => {
+    //             onDeleteWorkout(workout.id)
+    //         })
+    //         .catch((error) => console.log(error))
+    // }
+
+//     return (
+//         <div>
+//             <h3>Date:{workout.date}</h3>
+//             <div className='workout-item-weight'>
+//                 <h4>Weight:{workout.weight}</h4>
+//             </div>
+//             <div className='workout-item-buttons'>
+//                 <button onClick={performDeleteWorkout}>Delete</button>
+//                 {/* <button onClick={performEditWorkout}>Edit</button> */}
+//                 {/* <EditWorkout workout={workout} /> */}
+//                 <Link to={`/workouts/${workout.id}`}>Edit</Link>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default WorkoutItem
