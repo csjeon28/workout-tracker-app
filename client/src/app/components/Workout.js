d5baf2cf9e7e9fd089e49421f6bc6e251f9486b2
Christine Jeon <csjeon28@gmail.com>
2021-08-27 15:06:33 -0400

    patch and delete workout fetch requests

--- client/src/app/components/Workout.js
+++ client/src/app/components/Workout.js
@@ -5,25 +5,18 @@ const Workout = ({ workout, workouts, setWorkouts }) => {
     const [work, setWork] = useState([])
     const [editing, setEditing] = useState(false)
 
-    const onDeleteWorkout = (id) => {
-        const patchedWorkouts = workouts.filter(workout => workout.id !== id)
-        setWorkouts(patchedWorkouts)
-    }
-
     useEffect(() => {
-        // debugger
         fetch(`/workouts`)
             .then(resp => resp.json())
-            // .then(resp => console.log(resp))
             .then(data => setWork(data))
     }, [])
 
-    const deleteWorkout = () => {
+    const deleteWorkout = (id) => {
         fetch(`/workouts/${workout.id}`, {
             method: 'DELETE'
         })
             .then(() => {
-                onDeleteWorkout(workout.id)
+                setWorkouts(workouts.filter(workout => { return workout.id !== id }))
             })
     }
 
@@ -35,23 +28,23 @@ const Workout = ({ workout, workouts, setWorkouts }) => {
             },
             body: JSON.stringify(content)
         }
-        // debugger
         fetch(`/workouts/${workout.id}`, config)
             .then(resp => resp.json())
             .then(data => {
-                setWork(workouts.map(singleWorkout => singleWorkout.id === workout.id ? data : singleWorkout))
+                setWork(workouts.map(singleWorkout => singleWorkout.id === workouts.id ? data : singleWorkout))
             })
         setEditing(false)
     }
 
+    // debugger
     return (
         <div>
             <div>
                 <h3>Date:{workout.date}</h3>
                 <h4>Weight:{workout.weight}</h4>
-                {editing ? <EditWorkoutForm work={work} setWork={setWork} editWorkout={editWorkout} workout={workout} setEditing={setEditing} />
+                {editing ? <EditWorkoutForm work={work} editWorkout={editWorkout} workouts={workouts} setEditing={setEditing} />
                     : <button onClick={() => setEditing(true)}>Edit</button>}
-                <button onClick={deleteWorkout}>Delete</button>
+                <button onClick={() => deleteWorkout(workout.id)}>Delete</button>
             </div>
         </div>
     )
