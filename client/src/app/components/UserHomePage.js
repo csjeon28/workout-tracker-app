7550648ea7bbbb9e17a38598c6e74b8e95342b19
Christine Jeon <csjeon28@gmail.com>
2021-08-27 12:56:50 -0400

    userhomepage done

--- client/src/app/components/UserHomePage.js
+++ client/src/app/components/UserHomePage.js
@@ -1,28 +1,13 @@
-// import CreateWorkout from './CreateWorkout'
 import NavBar from './NavBar'
 import WorkoutsList from './WorkoutsList'
 
 const UserHomePage = ({ currentUser, setCurrentUser }) => {
-    // const UserHomePage = ({ handleNewWorkout, errors, workouts, setWorkouts, currentUser, setCurrentUser }) => {
-    // const UserHomePage = ({ errors, handleUserLoginAndSignup, handleNewWorkout, workouts, setWorkouts, currentUser, setCurrentUser }) => {
-
-    // const onDeleteWorkout = (id) => {
-    //     const patchedWorkouts = workouts.filter(workout => workout.id !== id)
-    //     setWorkouts(patchedWorkouts)
-    // }
 
     return (
         <div>
             <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser} />
             <h1>Your Workout Tracker</h1>
             <WorkoutsList />
-            {/* <WorkoutList
-                errors={errors}
-                handleNewWorkout={handleNewWorkout}
-                currentUser={currentUser}
-                onDeleteWorkout={onDeleteWorkout}
-                setWorkouts={setWorkouts}
-                workouts={workouts} /> */}
         </div>
     )
 }
