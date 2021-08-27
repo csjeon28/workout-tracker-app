246503356c5b3291d025a02ad470b996b46d12c4
Christine Jeon <csjeon28@gmail.com>
2021-08-27 14:02:44 -0400

    POST workouts functioning both frontend and backend

--- client/src/app/components/NewWorkoutForm.js
+++ client/src/app/components/NewWorkoutForm.js
@@ -1,31 +1,27 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const NewWorkoutForm = ({ addWorkout }) => {
-    const [date, setDate] = useState('')
-    const [weight, setWeight] = useState('')
-
-    // const [form, setForm] = useState({})
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target
-    //     setForm({ ...form, [name]: value })
-    // }
+    const history = useHistory()
+    const [form, setForm] = useState({})
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setForm({ ...form, [name]: value })
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        addWorkout({
-            date: date,
-            weight: weight
-        })
+        addWorkout(form)
+        history.push('/userhomepage')
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label>Date</label>
-                <input name='date' id='date' type='text' onChange={(e) => setDate(e.target.value)} />
+                <input name='date' id='date' type='text' onChange={handleChange} />
                 <label>Weight</label>
-                <input name='weight' id='weight' type='text' onChange={(e) => setWeight(e.target.value)} />
-                {/* <button>Add Workout</button> */}
+                <input name='weight' id='weight' type='text' onChange={handleChange} />
                 <input type='submit' value='Add Workout' />
             </form>
         </div>
