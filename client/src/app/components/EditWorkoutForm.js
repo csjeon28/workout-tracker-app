149964da4d160db48da5fb01fb086d88557bead5
Christine Jeon <csjeon28@gmail.com>
2021-08-27 21:01:23 -0400

    edit form not fully connected to backend. proxy issues

--- client/src/app/components/EditWorkoutForm.js
+++ client/src/app/components/EditWorkoutForm.js
@@ -1,37 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
-const EditWorkoutForm = ({ work, setWork, editWorkout, workout, setEditing }) => {
+const EditWorkoutForm = ({ workout, workouts, setWorkouts, setEditing }) => {
+    // const EditWorkoutForm = ({ workout, work, setWorkout, setWork, editWorkout, setEditing }) => {
     // const [date, setDate] = useState('')
     // const [weight, setWeight] = useState('')
-
-    // const setDateAndWeight = () => {
-    //     setDate(workout.date)
-    //     setWeight(workout.weight)
-    // }
+    const { id } = workout
+    const [form, setForm] = useState({ date: workout.date, weight: workout.weight })
 
     const handleChange = (e) => {
         const { name, value } = e.target
-        setWork({ ...work, [name]: value })
+        setForm({ ...form, [name]: value })
     }
-
-    // useEffect(setDateAndWeight)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        editWorkout({
-            date: work.date,
-            weight: work.weight
-        })
+
+        const config = {
+            method: 'PATCH',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(form)
+        }
+        fetch(`/workouts/${workout.id}`, config)
+            .then(resp => resp.json())
+            // .then(data => console.log(data))
+            .then(() => {
+                // setWork(data)
+                setWorkouts(workouts.map(w => w.id === id ? workout : w))
+            })
+        setEditing(false)
     }
+    // debugger
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label>Date</label>
-                <input name='date' id='date' type='text' onChange={handleChange} />
+                <input name='date' id='date' type='text' onChange={handleChange} value={form.date} />
                 {/* <input name='date' id='date' type='text' onChange={(e) => setDate(e.target.value)} /> */}
                 <label>Weight</label>
-                <input name='weight' id='weight' type='text' onChange={handleChange} />
+                <input name='weight' id='weight' type='text' onChange={handleChange} value={form.weight} />
                 {/* <input name='weight' id='weight' type='text' onChange={(e) => setWeight(e.target.value)} /> */}
                 <input type='submit' value='Add Edit' />
                 <button onClick={() => setEditing(false)}>Cancel</button>
