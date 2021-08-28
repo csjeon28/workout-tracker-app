324cccaf06ef7cdf19eeb90017e2173c1ed48738
Christine Jeon <csjeon28@gmail.com>
2021-08-27 21:00:02 -0400

    delete fetch request done, just need to debug patch request

--- client/src/app/components/Workout.js
+++ client/src/app/components/Workout.js
@@ -1,48 +1,57 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+// import { useHistory } from 'react-router-dom'
 import EditWorkoutForm from './EditWorkoutForm'
 
 const Workout = ({ workout, workouts, setWorkouts }) => {
-    const [work, setWork] = useState([])
+    // const [work, setWork] = useState([])
+    // const history = useHistory()
     const [editing, setEditing] = useState(false)
 
-    useEffect(() => {
-        fetch(`/workouts`)
-            .then(resp => resp.json())
-            .then(data => setWork(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch(`/workouts`)
+    //         .then(resp => resp.json())
+    //         .then(data => setWork(data))
+    //     debugger
+    // }, [])
 
     const deleteWorkout = (id) => {
         fetch(`/workouts/${workout.id}`, {
             method: 'DELETE'
         })
             .then(() => {
-                setWorkouts(workouts.filter(workout => { return workout.id !== id }))
+                setWorkouts(workouts.filter(workout => workout.id !== id))
             })
+        // history.push('/userhomepage')
     }
 
-    const editWorkout = (content) => {
-        const config = {
-            method: 'PATCH',
-            header: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(content)
-        }
-        fetch(`/workouts/${workout.id}`, config)
-            .then(resp => resp.json())
-            .then(data => {
-                setWork(workouts.map(singleWorkout => singleWorkout.id === workouts.id ? data : singleWorkout))
-            })
-        setEditing(false)
-    }
+    // const editWorkout = (edit) => {
+    //     const config = {
+    //         method: 'PATCH',
+    //         header: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(edit)
+    //     }
+    //     // debugger
+    //     fetch(`/workouts/${workout.id}`, config)
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             setWork(data)
+    //             // setWork(workouts.map(editForm => editForm.id === workout.id ? workout : editForm))
+    //         })
+    //     setEditing(false)
+    // }
 
     // debugger
     return (
         <div>
             <div>
-                <h3>Date:{workout.date}</h3>
-                <h4>Weight:{workout.weight}</h4>
-                {editing ? <EditWorkoutForm work={work} editWorkout={editWorkout} workouts={workouts} setEditing={setEditing} />
+                <div key={workout.id}>
+                    <h3>Date:{workout.date}</h3>
+                    <h4>Weight:{workout.weight}</h4>
+                </div>
+                {editing ? <EditWorkoutForm workout={workout} workouts={workouts} setWorkouts={setWorkouts} setEditing={setEditing} />
+                    // {editing ? <EditWorkoutForm work={work} setWork={setWork} workout={workout} editWorkout={editWorkout} setEditing={setEditing} />
                     : <button onClick={() => setEditing(true)}>Edit</button>}
                 <button onClick={() => deleteWorkout(workout.id)}>Delete</button>
             </div>
