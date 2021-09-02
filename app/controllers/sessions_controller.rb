d980ca102cc3ad2b69e1d59bfbf48a1b97082959
Christine Jeon <csjeon28@gmail.com>
2021-09-02 17:08:54 -0400

    cleaned up code and added exercises

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,19 @@
 class SessionsController < ApplicationController
     skip_before_action :authorize, only: :create
 
-    # POST /login (sessions#create)
+    #----------POST /login (sessions#create)------------
     def create
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            render json: {user: user}, status: :ok
+            render json: {user: user, workouts: user.workouts, exercises: user.exercises}, status: :ok
         else
             render json: {errors: ["Invalid username or password"]}, status: :unauthorized
         end
     end
 
-    # DELETE /logout (sessions#destroy)
+    #-----------DELETE /logout (sessions#destroy)------------
     def destroy
-        # if session[:user_id]
-        #     session.delete :user_id
-        # else
-        #     render json: {errors: ["You are not logged in"]}, status: :unauthorized
-        # end
         session.delete :user_id
         head :no_content
     end
