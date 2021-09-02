86daf9d7145d0bd693650de514607cd688ee73a0
Christine Jeon <csjeon28@gmail.com>
2021-09-02 18:00:50 -0400

    added exercises method to list both workouts&exercises to user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,4 +10,16 @@ class UserSerializer < ActiveModel::Serializer
       }
     end
   end
+
+  def exercises
+    self.object.exercises.map do |exercise_obj|
+      {
+        id: exercise_obj.id,
+        name: exercise_obj.name,
+        calories: exercise_obj.calories,
+        duration: exercise_obj.duration
+      }
+    end
+  end
+  
 end
