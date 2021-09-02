b3116a24c75232593fff0552b06b6a5a050c2afd
Christine Jeon <csjeon28@gmail.com>
2021-09-02 17:49:09 -0400

    added current_user instance method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    # include ActionController::Serialization
     rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     before_action :authorize
   
     private
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::API
 
     def authorize
       if !session[:user_id]
-        render json: { errors: ['Not logged in'] }, status: :unauthorized
+        render json: { errors: ['Not authorized'] }, status: :unauthorized
       else
-        @current_user = User.find_by(id: session[:user_id])
+        @current_user = User.find(session[:user_id])
       end
     end
   
@@ -22,4 +22,7 @@ class ApplicationController < ActionController::API
       render json: { errors: e.record.errors.full_messages }, status: 400
     end
   
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
