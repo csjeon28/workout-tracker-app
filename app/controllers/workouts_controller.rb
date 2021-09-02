eb8c4157f3024c6e64ba8d5205f390102a4ff299
Christine Jeon <csjeon28@gmail.com>
2021-09-02 17:48:42 -0400

    finally got ppatch request to function correctly and cleaned up code (took out byebugs)

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,67 +1,47 @@
 class WorkoutsController < ApplicationController
-    # skip_before_action :authorize
     before_action :authorize, :current_user
-    skip_before_action :authorize, only: [:index, :create, :update, :destroy]
-    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    before_action :authorize, only: [:show, :create, :update, :destroy]
 
+    #----------GET /workouts (workouts#index)------------
     def index
         if session[:user_id]
-            workouts = @current_user.workouts
+            workouts = show_workouts
             render json: workouts, include: [:exercise]
         end
-        # byebug
-        # workouts = @current_user.workouts
-        # options = {include: [:user, :exercise]}
-        # json = WorkoutSerializer.new(workouts, options).serializable_hash.to_json
-        # render json: json, status: :ok
     end
 
+    #-----------POST /workouts (workouts#create)------------
     def create
-        # byebug
         if session[:user_id]
-            workout = @current_user.workouts.create(workout_params)
+            workout = show_workouts.create(workout_params)
             workout.update!(user_id: session[:user_id])
-            render json: {workout: workout}, include: [:user], status: :created
+            render json: {workout: workout}, status: :created
         else
             render json: { errors: ['You must be logged in to create a workout'] }, status: :unauthorized
         end
     end
 
+    #-----------GET /workouts/:id (workouts#show)-------------
     def show
         if session[:user_id]
             workout = find_workout
-            # exercises = workout.exercises
-            # render json: {workout: workout, exercises: exercises}
-        # workout = find_workout
-        render json: workout
+            render json: {workout: workout}
         end
     end
 
+    #-----------PATCH /workouts/:id (workouts#update)-------------
     def update
-        # FIGURE OUT WHY UPDATE PARAMS ISN'T ALLOWING WORKOUT AS A PARAM
-        workout = find_workout
-        if @current_user.id == session[:user_id]
+        workout = Workout.find(params[:id])
+        if workout[:user_id] == session[:user_id]
             workout.update(workout_params)
-            render json: workout
+            render json: {workout: workout}, status: :accepted
+        else
+            render json: {errors: ["Not authorized"]}, status: :unauthorized
         end
-        # workout = find_workout
-        # workout.update(workout_params)
-        # render json: workout
-
-        # workout = find_workout
-        # workout.update!(workout_params)
-        # render json: workout
-
-        # workout = find_workout
-        # if workout[:user_id] == session[:user_id]
-        #     workout.update(workout_params)
-        #     render json: {workout: workout}, include: ['user'], status: :accepted
-        # end
-        
     end
 
+    #----------DELETE /workouts/:id (workouts#destroy)---------------
     def destroy
-        # byebug
         if session[:user_id]
             workout = find_workout
             workout.destroy
@@ -71,27 +51,20 @@ class WorkoutsController < ApplicationController
 
     private
 
+    def show_workouts
+        @current_user.workouts
+    end
+
     def find_workout
-        # Workout.find(params[:id])
-        @current_user.workouts.find_by(params[:id])
+        @current_user.workouts.find(params[:id])
     end
 
     def workout_params
-        # params.permit(:date, :weight)
-        params.permit(:id, :date, :weight)
-        # params.require(:workout).permit(:id, :date, :weight, :user_id)
+        params.permit(:id, :user_id, :date, :weight, exercise: [:name, :calories, :duration])
     end
 
     def authorize
         render json: { errors: ['Not authorized'] }, status: :unauthorized unless session.include? :user_id
     end
-    # def authorize
-    #     return render json: { errors: ['Not authorized'] }, status: :unauthorized unless session.include? :user_id
-    # end
-
-    # def render_unprocessable_entity_response(invalid)
-    #     render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
-    # end
-
 
 end
