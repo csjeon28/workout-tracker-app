1bc6b226a209baf7faf4a188266aceec55317715
Christine Jeon <csjeon28@gmail.com>
2021-09-02 17:58:03 -0400

    added index, create, show, update, destroy methods similar to workouts controller to set up

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,28 +1,70 @@
 class ExercisesController < ApplicationController
-    skip_before_action :authorize, only: [:index]
+    before_action :authorize, :current_user
+    before_action :authorize, only: [:show, :create, :update, :destroy]
 
+    #----------GET /exercises (exercises#index)------------   
     def index
         if session[:user_id]
-            exercises = Exercise.all
+            exercises = show_exercises
             render json: exercises
         end
     end
 
+    #-----------POST /exercises (exercises#create)------------
     def create
-        exercise = @current_user.exercises.build(exercise_params)
-        if exercise.save!
-            render json: {exercises: exercise}, status: 201
+        if session[:user_id]
+            exercise = show_exercises.create(exercise_params)
+            exercise.update!(user_id: session[:user_id])
+            render json: {exercise: exercise}, status: :created
+        else
+            render json: { errors: ['You must be logged in to create a exercise'] }, status: :unauthorized
         end
     end
 
+    #-----------GET /exercises/:id (exercises#show)-------------
     def show
+        if session[:user_id]
+            exercise = find_exercise
+            render json: {exercise: exercise}
+        end
+    end
+
+    #-----------PATCH /exercises/:id (exercises#update)-------------
+    def update
         exercise = Exercise.find(params[:id])
-        render json: exercise
+        if exercise[:user_id] == session[:user_id]
+            exercise.update(exercise_params)
+            render json: {exercise: exercise}, status: :accepted
+        else
+            render json: {errors: ["Not authorized"]}, status: :unauthorized
+        end
+    end
+
+    #----------DELETE /exercises/:id (exercises#destroy)---------------
+    def destroy
+        if session[:user_id]
+            exercise = find_exercise
+            exercise.destroy
+            head :no_content
+        end
     end
 
     private
 
+    def show_exercises
+        @current_user.exercises
+    end
+
+    def find_exercise
+        @current_user.exercises.find(params[:id])
+    end
+    
     def exercise_params
-        params.permit(:name, :calories, :duration, :user_id)
+        params.permit(:name, :calories, :duration, :user_id, :workout_id)
+        # params.require(:exercise).permit(:name, :calories, :duration)
+    end
+
+    def authorize
+        render json: { errors: ['Not authorized'] }, status: :unauthorized unless session.include? :user_id
     end
 end
