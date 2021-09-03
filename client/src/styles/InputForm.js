f8d44d56b26966ea0ed923163a0003d35cc16d4e
Christine Jeon <csjeon28@gmail.com>
2021-09-03 15:56:57 -0400

    recommented in styled components after getting functionality to work

--- client/src/styles/InputForm.js
+++ client/src/styles/InputForm.js
@@ -1,14 +1,14 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
-// const InputForm = styled.form`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: space-around;
-//   align-items: center;
-//   height: 70%;
-//   width: 100%;
-//   margin: 1rem 0 2rem 0;
-//   align-items: center;
-// `;
+const InputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 70%;
+  width: 100%;
+  margin: 1rem 0 2rem 0;
+  align-items: center;
+`;
 
-// export default InputForm
+export default InputForm
