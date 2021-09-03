faa7bdabc472001e953cfe0346b87f4964ab74d8
Christine Jeon <csjeon28@gmail.com>
2021-09-03 16:33:00 -0400

    finished logout with styled components

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { MainContainer, HorizontalLine } from '../styles'
 
-const Logout = ({ setCurrentUser, setWorkouts }) => {
+const Logout = ({ setCurrentUser }) => {
     const history = useHistory()
 
     useEffect(() => {
@@ -14,15 +16,27 @@ const Logout = ({ setCurrentUser, setWorkouts }) => {
 
     const handleLogout = () => {
         setCurrentUser(null)
-        setWorkouts([])
-        history.push('/login')
+        setTimeout(() => {
+            history.push('/login')
+        }, 2000)
     }
 
     return (
-        <div>
-            <h1>Logged Out</h1>
-        </div>
+        <MainContainer>
+            <br />
+            <br />
+            <HorizontalLine />
+            <LogoutText>Logging Out...</LogoutText>
+            <HorizontalLine />
+        </MainContainer>
     )
 }
+
+const LogoutText = styled.h1`
+  font-size: 1.5rem;
+  letter-spacing: 0.4rem;
+  margin: 5rem 0 5rem 0;
+  line-height: 1;
+`;
 
 export default Logout
