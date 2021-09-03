3971ee43a132b2042bd7519fcb539fbd3a634fec
Christine Jeon <csjeon28@gmail.com>
2021-09-02 20:18:25 -0400

    post request working and cleaned up return

--- client/src/app/components/WorkoutsList.js
+++ client/src/app/components/WorkoutsList.js
@@ -11,26 +11,25 @@ const WorkoutsList = () => {
             .then(data => setWorkouts(data))
     }, [])
 
-    const addWorkout = (workout) => {
+    const addWorkout = (data) => {
         const config = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(workout)
+            body: JSON.stringify(data)
         }
         fetch('/workouts', config)
             .then(resp => resp.json())
-            .then(data => setWorkouts([...workouts, data.workout]))
-        // debugger
+            .then(() => setWorkouts([...workouts, data]))
     }
-    // const workoutList = workouts.map((workout, index) => <WorkoutLink key={index} workout={workout} />)
-    // const workoutList = workouts.map((workout, index) => <Workout key={index} workout={workout} workouts={workouts} setWorkouts={setWorkouts} />)
-    // debugger
+
+    const workoutList = workouts.map((workout, index) => <Workout key={index} workout={workout} workouts={workouts} setWorkouts={setWorkouts} />)
+
     return (
         <div>
             <NewWorkoutForm addWorkout={addWorkout} />
-            {workouts.map((workout, index) => <Workout key={index} workout={workout} workouts={workouts} setWorkouts={setWorkouts} />)}
+            {workoutList}
         </div>
     )
 }
