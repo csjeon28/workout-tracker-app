4d4c828c437c2814eeb8c24b8b9a0b174d0e3580
Christine Jeon <csjeon28@gmail.com>
2021-09-02 20:23:44 -0400

    set up code for exercise delete

--- client/src/app/components/ExerciseLink.js
+++ client/src/app/components/ExerciseLink.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import EditExerciseForm from './EditExerciseForm'
 
-const ExerciseLink = ({ exercise }) => {
+const ExerciseLink = ({ exercises, setExercises, exercise }) => {
+    const [editing, setEditing] = useState(false)
+
+    const deleteExercise = (id) => {
+        fetch(`/exercises/${exercise.id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                setExercises(exercises.filter(exercise => exercise.id !== id))
+            })
+    }
 
     return (
         <div>
+            <div key={exercise.id}>
+                <h3>Name:{exercise.name}</h3>
+            </div>
             <Link to={`/exercises/${exercise.id}`}>
                 <h3>{exercise.name}</h3>
             </Link>
+            {editing ? <EditExerciseForm setEditing={setEditing} />
+                // {editing ? <EditWorkoutForm workout={workout} workouts={workouts} setWorkouts={setWorkouts} setEditing={setEditing} />
+                // {editing ? <EditWorkoutForm work={work} setWork={setWork} workout={workout} editWorkout={editWorkout} setEditing={setEditing} />
+                : <button onClick={() => setEditing(true)}>Add Exercise</button>}
+            <button onClick={() => deleteExercise(exercise.id)}>Delete</button>
         </div>
     )
 }
