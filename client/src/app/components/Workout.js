2788c571f3f88a3b0284885589b31febfbe75ada
Christine Jeon <csjeon28@gmail.com>
2021-09-02 20:22:39 -0400

    took out unnecessary code and need to add exercise list in return

--- client/src/app/components/Workout.js
+++ client/src/app/components/Workout.js
@@ -1,48 +1,20 @@
 import React, { useState } from 'react'
-// import { useHistory } from 'react-router-dom'
+// import ExerciseList from './ExerciseList'
 import EditWorkoutForm from './EditWorkoutForm'
 
 const Workout = ({ workout, workouts, setWorkouts }) => {
-    // const [work, setWork] = useState([])
-    // const history = useHistory()
     const [editing, setEditing] = useState(false)
 
-    // useEffect(() => {
-    //     fetch(`/workouts`)
-    //         .then(resp => resp.json())
-    //         .then(data => setWork(data))
-    //     debugger
-    // }, [])
-
     const deleteWorkout = (id) => {
+        debugger
         fetch(`/workouts/${workout.id}`, {
             method: 'DELETE'
         })
             .then(() => {
-                setWorkouts(workouts.filter(workout => workout.id !== id))
+                setWorkouts(workouts.filter(w => w.id !== id))
             })
-        // history.push('/userhomepage')
     }
 
-    // const editWorkout = (edit) => {
-    //     const config = {
-    //         method: 'PATCH',
-    //         header: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(edit)
-    //     }
-    //     // debugger
-    //     fetch(`/workouts/${workout.id}`, config)
-    //         .then(resp => resp.json())
-    //         .then(data => {
-    //             setWork(data)
-    //             // setWork(workouts.map(editForm => editForm.id === workout.id ? workout : editForm))
-    //         })
-    //     setEditing(false)
-    // }
-
-    // debugger
     return (
         <div>
             <div>
@@ -50,8 +22,7 @@ const Workout = ({ workout, workouts, setWorkouts }) => {
                     <h3>Date:{workout.date}</h3>
                     <h4>Weight:{workout.weight}</h4>
                 </div>
-                {editing ? <EditWorkoutForm workout={workout} workouts={workouts} setWorkouts={setWorkouts} setEditing={setEditing} />
-                    // {editing ? <EditWorkoutForm work={work} setWork={setWork} workout={workout} editWorkout={editWorkout} setEditing={setEditing} />
+                {editing ? <EditWorkoutForm workouts={workouts} setWorkouts={setWorkouts} workout={workout} setEditing={setEditing} />
                     : <button onClick={() => setEditing(true)}>Edit</button>}
                 <button onClick={() => deleteWorkout(workout.id)}>Delete</button>
             </div>
