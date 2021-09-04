245cd18f1b425d1f14fdb94c8bf650e9a893ea39
Christine Jeon <csjeon28@gmail.com>
2021-09-03 22:16:20 -0400

    adding exercise form

---
+++ client/src/components/AddExerciseForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const AddExerciseForm = ({ addExercise }) => {
+
+    const [form, setForm] = useState({})
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setForm({ ...form, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addExercise(form)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input name='name' type='text' onChange={handleChange} />
+                <label>Active Calories</label>
+                <input name='calories' type='text' onChange={handleChange} />
+                <label>Duration</label>
+                <input name='duration' type='text' onChange={handleChange} />
+                <input type='submit' value='Add Exercise' />
+            </form>
+        </div>
+    )
+}
+
+export default AddExerciseForm
