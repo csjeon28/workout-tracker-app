104e1b738d6b1f9b031b82cbe727af38e080d13c
Christine Jeon <csjeon28@gmail.com>
2021-09-04 19:28:44 -0400

    added more styled comps to index

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,12 +1,29 @@
-import AddContainer from './AddContainer'
 import Button from './Button'
 import FormField from './FormField'
 import HorizontalLine from './HorizontalLine'
 import Input from './Input'
 import InputForm from './InputForm'
 import LoginButton from './LoginButton'
+import LoginPageBtn from './LoginPageBtn'
 import Logo from './Logo'
 import MainContainer from './MainContainer'
 import SignupButton from './SignupButton'
+import SignInput from './SignInput'
+import SignupPageBtn from './SignupPageBtn'
+import SmallContainer from './SmallContainer'
 
-export { AddContainer, Button, FormField, HorizontalLine, Input, InputForm, LoginButton, Logo, MainContainer, SignupButton }
+export {
+    Button,
+    FormField,
+    HorizontalLine,
+    Input,
+    InputForm,
+    LoginButton,
+    LoginPageBtn,
+    Logo,
+    MainContainer,
+    SignupButton,
+    SignInput,
+    SignupPageBtn,
+    SmallContainer
+}
