5dac0f092dd797ec04c0dd7acb6517efdd00905b
Christine Jeon <csjeon28@gmail.com>
2021-09-04 19:28:30 -0400

    added styled comp for login btn on signup page

---
+++ client/src/styles/LoginPageBtn.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+function LoginPageBtn({ type, content }) {
+    return <LoginPageBtnStyle type={type}>{content}</LoginPageBtnStyle>
+}
+
+const LoginPageBtnStyle = styled.button`
+  background: linear-gradient(to right, #ffffff 0%, #ffe7d1 70%);
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  width: 30%;
+  height: 2rem;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
+  color: #2b3099;
+  font-weight: bold;
+  margin: 0.3rem 0 0 0;
+`;
+
+export default LoginPageBtn
