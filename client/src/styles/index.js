fb19c86ea5af80c6bf1104635dcb40793c06cbeb
Christine Jeon <csjeon28@gmail.com>
2021-09-03 22:21:33 -0400

    imported new styled components

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,10 +1,12 @@
+import AddContainer from './AddContainer'
 import Button from './Button'
 import FormField from './FormField'
 import HorizontalLine from './HorizontalLine'
 import Input from './Input'
 import InputForm from './InputForm'
 import LoginButton from './LoginButton'
+import Logo from './Logo'
 import MainContainer from './MainContainer'
 import SignupButton from './SignupButton'
 
-export { Button, FormField, HorizontalLine, Input, InputForm, LoginButton, MainContainer, SignupButton }
+export { AddContainer, Button, FormField, HorizontalLine, Input, InputForm, LoginButton, Logo, MainContainer, SignupButton }
