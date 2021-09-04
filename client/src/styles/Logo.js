b00843f8d9ec1876aa2720d59c014b22828c4d42
Christine Jeon <csjeon28@gmail.com>
2021-09-03 22:21:50 -0400

    added styled component for h1 tag

---
+++ client/src/styles/Logo.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Logo = styled.h1`
+  font-size: 1rem;
+  text-transform: uppercase;
+  letter-spacing: 0.4rem;
+  color: white;
+  margin: 0;
+  line-height: 1;
+`;
+
+export default Logo
