f9a8d8c67a62c88a060c95e86cda3ef0b558ecb3
Christine Jeon <csjeon28@gmail.com>
2021-09-04 19:52:10 -0400

    styled errors to stay within screen and not mess up other CSS

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -5,63 +5,65 @@ import styled from 'styled-components'
 import { MainContainer, HorizontalLine, InputForm, LoginPageBtn, SignInput, SignupPageBtn } from '../styles'
 
 const Signup = ({ errors, handleUserLoginAndSignup }) => {
-    const history = useHistory()
-    const [state, setState] = useState({})
+  const history = useHistory()
+  const [state, setState] = useState({})
 
-    const onChange = (e) => {
-        const { name, value } = e.target
-        setState({ ...state, [name]: value })
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setState({ ...state, [name]: value })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(state)
     }
+    fetch('/users', config)
+      .then(resp => resp.json())
+      .then(data => handleUserLoginAndSignup(data))
+  }
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-
-        const config = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(state)
-        }
-        fetch('/users', config)
-            .then(resp => resp.json())
-            .then(data => handleUserLoginAndSignup(data))
-    }
-
-    return (
-        <MainContainer>
-            <HorizontalLine />
-            <LogoText>Sign up to track your workouts each day!</LogoText>
-            <HorizontalLine />
-            <br />
-            <List>Sign-In Requirements:
-                <ListItem>Username must be at least 5 characters</ListItem>
-                <ListItem>Password must contain:
-                    <UnorderedList>
-                        <UnorderedItem>At least 1 number</UnorderedItem>
-                        <UnorderedItem>At least 1 lowercase letter</UnorderedItem>
-                        <UnorderedItem>At least 1 uppercase letter</UnorderedItem>
-                        <UnorderedItem>At least 1 symbol</UnorderedItem>
-                        <UnorderedItem>Minimum 8 characters</UnorderedItem>
-                    </UnorderedList>
-                </ListItem>
-            </List>
-            <InputForm onSubmit={onSubmit}>
-                <LabelText>Create Username:</LabelText>
-                <SignInput onChange={onChange} name='username' type='text' />
-                <LabelText>Create Password:</LabelText>
-                <SignInput onChange={onChange} name='password' type='password' />
-                <ConfirmText>Confirm Password:</ConfirmText>
-                <SignInput onChange={onChange} name='password_confirmation' type='password' />
-                <SignupPageBtn type='submit' content='Signup' onClick={() => history.push('/userhomepage')} />
-                <HorizontalLine />
-                <FooterText>Already have an account?</FooterText>
-                <LoginPageBtn type='submit' content='Log In' onClick={() => history.push('/login')} />
-            </InputForm>
-            <Errors errors={errors} />
-        </MainContainer>
-    )
+  return (
+    <MainContainer>
+      <HorizontalLine />
+      <LogoText>Sign up to track your workouts each day!</LogoText>
+      <HorizontalLine />
+      <br />
+      <List>Sign-In Requirements:
+        <ListItem>Username must be at least 5 characters</ListItem>
+        <ListItem>Password must contain:
+          <UnorderedList>
+            <UnorderedItem>At least 1 number</UnorderedItem>
+            <UnorderedItem>At least 1 lowercase letter</UnorderedItem>
+            <UnorderedItem>At least 1 uppercase letter</UnorderedItem>
+            <UnorderedItem>At least 1 symbol</UnorderedItem>
+            <UnorderedItem>Minimum 8 characters</UnorderedItem>
+          </UnorderedList>
+        </ListItem>
+      </List>
+      <InputForm onSubmit={onSubmit}>
+        <LabelText>Create Username:</LabelText>
+        <SignInput onChange={onChange} name='username' type='text' />
+        <LabelText>Create Password:</LabelText>
+        <SignInput onChange={onChange} name='password' type='password' />
+        <ConfirmText>Confirm Password:</ConfirmText>
+        <SignInput onChange={onChange} name='password_confirmation' type='password' />
+        <SignupPageBtn type='submit' content='Signup' onClick={() => history.push('/userhomepage')} />
+        <HorizontalLine />
+      </InputForm>
+      <FooterText>Already have an account?</FooterText>
+      <LoginPageBtn type='submit' content='Log In' onClick={() => history.push('/login')} />
+      <ErrorStyle>
+        <Errors errors={errors} />
+      </ErrorStyle>
+    </MainContainer>
+  )
 }
 
 const LogoText = styled.h2`
@@ -116,10 +118,19 @@ const ConfirmText = styled.h5`
 `;
 
 const FooterText = styled.h5`
-  margin: 0.4rem 0 0 0;
+  margin: -2rem 0 0 0;
   color: white;
   letter-spacing: 0.2rem;
   text-shadow: 1px 1px 1.3px #ffe7d1;
+`;
+
+const ErrorStyle = styled.div`
+  margin: 0.4rem 0 0.5rem 3rem;
+  text-shadow: 1px 1px 1px #7e7e9c;
+  font-weight: 500;
+  letter-spacing: 0.1rem;
+  font-size: 0.8rem;
+  text-align: left;
 `;
 
 export default Signup
