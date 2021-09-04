d942856a47096bc64790b4bcb014b1846b67fd82
Christine Jeon <csjeon28@gmail.com>
2021-09-03 22:20:21 -0400

    moved workout component over from another folder

---
+++ client/src/components/Workout.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import EditWorkoutForm from './EditWorkoutForm'
+
+const Workout = ({ workout, workouts, setWorkouts }) => {
+    const [editing, setEditing] = useState(false)
+
+    const deleteWorkout = (id) => {
+        debugger
+        fetch(`/workouts/${workout.id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                setWorkouts(workouts.filter(w => w.id !== id))
+            })
+    }
+
+    return (
+        <div>
+            <div>
+                <div key={workout.id}>
+                    <h3>Date:{workout.date}</h3>
+                    <h4>Weight:{workout.weight}</h4>
+                </div>
+                {editing ? <EditWorkoutForm workouts={workouts} setWorkouts={setWorkouts} workout={workout} editing={editing} setEditing={setEditing} />
+                    : <button onClick={() => setEditing(true)}>Edit</button>}
+                <button onClick={() => deleteWorkout(workout.id)}>Delete</button>
+            </div>
+        </div>
+    )
+
+}
+
+export default Workout
