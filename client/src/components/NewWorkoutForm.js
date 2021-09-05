9c462b72559d9318a1e6603c13b0f039b4c9d3af
Christine Jeon <csjeon28@gmail.com>
2021-09-05 01:39:44 -0400

    finished styling for new workouts form

--- client/src/components/NewWorkoutForm.js
+++ client/src/components/NewWorkoutForm.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+import { AddBtn, NewInput, NewInputForm } from '../styles'
 
 const NewWorkoutForm = ({ addWorkout }) => {
 
     const [form, setForm] = useState({})
+
     const handleChange = (e) => {
         const { name, value } = e.target
         setForm({ ...form, [name]: value })
@@ -15,15 +18,22 @@ const NewWorkoutForm = ({ addWorkout }) => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>Date</label>
-                <input name='date' id='date' type='text' onChange={handleChange} />
-                <label>Weight</label>
-                <input name='weight' id='weight' type='text' onChange={handleChange} />
-                <input type='submit' value='Add Workout' />
-            </form>
+            <NewInputForm onSubmit={handleSubmit}>
+                <FormText>Date:</FormText>
+                <NewInput name='date' type='date' onChange={handleChange} />
+                <FormText>Weight:</FormText>
+                <NewInput name='weight' type='number' onChange={handleChange} />
+                <AddBtn type='submit' content='Add Workout' />
+            </NewInputForm>
         </div>
     )
 }
+
+const FormText = styled.h5`
+  margin: 0.2rem 0 1rem 0;
+  text-shadow: 1px 1px 1px #7e7e9c;
+  letter-spacing: 0.05rem;
+  font-size: 0.8rem;
+`;
 
 export default NewWorkoutForm
