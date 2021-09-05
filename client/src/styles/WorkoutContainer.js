a984b761d1104c4351cc06abcbe61e80e8fba455
Christine Jeon <csjeon28@gmail.com>
2021-09-05 17:41:31 -0400

    added a separate container for workout comp

---
+++ client/src/styles/WorkoutContainer.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const WorkoutContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 7vh;
+  width: 30vw;
+  background: rgba(255, 255, 255, 0.18);
+  box-shadow: 0 4px 4px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(12px);
+  border-radius: 8px;
+  color: #8a8294;
+  font-size: 14px;
+  text-shadow: 1px 1px 1px #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+`;
+
+export default WorkoutContainer
