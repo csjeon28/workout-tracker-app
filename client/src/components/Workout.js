c93cd90714cc1c587bbafaf06bcdd3203bb8bcdd
Christine Jeon <csjeon28@gmail.com>
2021-09-05 17:39:12 -0400

    added styling

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import EditWorkoutForm from './EditWorkoutForm'
+import styled from 'styled-components'
+import { WorkoutContainer, EditBtn, DeleteBtn } from '../styles'
 
 const Workout = ({ workout, workouts, setWorkouts }) => {
     const [editing, setEditing] = useState(false)
@@ -15,18 +17,24 @@ const Workout = ({ workout, workouts, setWorkouts }) => {
 
     return (
         <div>
-            <div>
-                <div key={workout.id}>
-                    <h3>Date:{workout.date}</h3>
-                    <h4>Weight:{workout.weight}</h4>
-                </div>
-                {editing ? <EditWorkoutForm workouts={workouts} setWorkouts={setWorkouts} workout={workout} editing={editing} setEditing={setEditing} />
-                    : <button onClick={() => setEditing(true)}>Edit</button>}
-                <button onClick={() => deleteWorkout(workout.id)}>Delete</button>
-            </div>
+            <WorkoutContainer key={workout.id}>
+                <Date>{workout.date}</Date>
+                <Weight>Weight:{workout.weight}lbs</Weight>
+            </WorkoutContainer>
+            {editing ? <EditWorkoutForm workouts={workouts} setWorkouts={setWorkouts} workout={workout} editing={editing} setEditing={setEditing} />
+                : <EditBtn onClick={() => setEditing(true)} content='Edit' />}
+            <DeleteBtn onClick={() => deleteWorkout(workout.id)} content='Delete' />
         </div>
     )
 
 }
+
+const Date = styled.h3`
+  letter-spacing: 0.3rem;
+`;
+
+const Weight = styled.h4`
+  text-transform: none;
+`;
 
 export default Workout
