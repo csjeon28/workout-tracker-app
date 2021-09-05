873e6eeaa951897251624d3a3d876e00d979baae
Christine Jeon <csjeon28@gmail.com>
2021-09-05 01:37:51 -0400

    styling for new workout form inputs

---
+++ client/src/styles/NewInputForm.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const NewInputForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: left;
+  height: 28%;
+  margin: 0.6rem 0.5rem 1.5rem -0.1rem;
+`;
+
+export default NewInputForm
