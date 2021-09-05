0843fc60de3563fade6684d39417831217e819f8
Christine Jeon <csjeon28@gmail.com>
2021-09-05 01:37:30 -0400

    input styling for new workouts

---
+++ client/src/styles/NewInput.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+function NewInput({ type, onChange, name }) {
+    return <NewInputStyle type={type} onChange={onChange} name={name} />
+}
+
+const NewInputStyle = styled.input`
+  background: rgba(255, 255, 255, 0.24);
+  box-shadow: 0 2px 4px 0 rgba(31, 38, 135, 0.37);
+  border-radius: 0.5rem;
+  width: 38%;
+  height: 1rem;
+  padding: 0.7rem;
+  border: none;
+  outline: none;
+  color: #3c354e;
+  font-size: 0.8rem;
+  font-weight: bold;
+  &:focus {
+    display: inline-block;
+    box-shadow: 0 0 0 0.2rem #b9abe0;
+    backdrop-filter: blur(12rem);
+    border-radius: 2rem;
+  }
+  &::placeholder {
+    color: #8773bf;
+    font-weight: 100;
+    font-size: 1rem;
+  }
+`;
+
+export default NewInput
