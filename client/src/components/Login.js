27402df4049b98b9393fc11d273de39125f823a3
Christine Jeon <csjeon28@gmail.com>
2021-09-05 01:39:18 -0400

    changed names for some styled-components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import Errors from './Errors'
 import styled from 'styled-components'
-import { MainContainer, Input, InputForm, HorizontalLine, LoginButton, SignupButton } from '../styles'
+import { MainContainer, ErrorStyle, Input, InputForm, HorizontalLine, LoginButton, LoginPageBtn } from '../styles'
 
 const Login = ({ errors, handleUserLoginAndSignup }) => {
     const history = useHistory()
@@ -33,7 +33,7 @@ const Login = ({ errors, handleUserLoginAndSignup }) => {
     return (
         <MainContainer>
             <InputForm onSubmit={onSubmit}>
-                <LogoText>Workout Tracker</LogoText>
+                <Header>Workout Tracker</Header>
                 <Input placeholder='Username' onChange={onChange} name='username' type='text' />
                 <br />
                 <Input placeholder='Password' onChange={onChange} name='password' type='password' />
@@ -42,19 +42,27 @@ const Login = ({ errors, handleUserLoginAndSignup }) => {
                 <br />
                 <HorizontalLine />
                 <SignUpText>New User?</SignUpText>
-                <SignupButton type='submit' content='Sign Up' onClick={() => history.push('/signup')} />
+                <LoginPageBtn type='submit' content='Sign Up' onClick={() => history.push('/signup')} />
             </InputForm>
-            <Errors errors={errors} />
+            <ErrorStyle>
+                <Errors errors={errors} />
+            </ErrorStyle>
         </MainContainer>
     )
 }
 
-const LogoText = styled.h2`
+const Header = styled.h2`
   margin: 3rem 0 2rem 0;
+  padding: 1rem;
+  text-shadow: 1.2px 0.4px 1.2px #062c9e;
+  text-align: center;
 `;
 
 const SignUpText = styled.h4`
-  margin: 2rem 0 2rem 0;
+  margin: 2rem 0 0 0;
+  color: white;
+  letter-spacing: 0.2rem;
+  text-shadow: 1px 1px 1.3px #ffe7d1;
 `;
 
 export default Login
