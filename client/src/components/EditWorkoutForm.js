ab574fc3b2814da4f5bbecdb193688604312b8a8
Christine Jeon <csjeon28@gmail.com>
2021-09-05 17:39:42 -0400

    added styling to form and buttons

--- client/src/components/EditWorkoutForm.js
+++ client/src/components/EditWorkoutForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { NewInputForm, NewInput, EditBtn, DeleteBtn } from '../styles'
 
 const EditWorkoutForm = ({ workout, setWorkouts, workouts, setEditing }) => {
     const { id } = workout
@@ -31,18 +33,46 @@ const EditWorkoutForm = ({ workout, setWorkouts, workouts, setEditing }) => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Date</label>
-                <input name='date' id='date' type='text' onChange={handleChange} value={form.date} />
-                <label>Weight</label>
-                <input name='weight' id='weight' type='text' onChange={handleChange} value={form.weight} />
-                <input type='submit' value='Add Edit' />
-                <button onClick={() => setEditing(false)}>Cancel</button>
-                <button onClick={() => history.push('/exercises')}>Add Exercise</button>
-            </form>
-        </div>
+        <NewInputForm onSubmit={handleSubmit}>
+            <FormText>Date</FormText>
+            <NewInput name='date' type='date' onChange={handleChange} value={form.date} />
+            <FormText>Weight</FormText>
+            <NewInput name='weight' type='number' onChange={handleChange} value={form.weight} />
+            <EditBtn type='submit' content='Add Changes' />
+            <DeleteBtn onClick={() => setEditing(false)} content='Cancel' />
+            <AddExercise onClick={() => history.push('/exercises')}>Add Exercise</AddExercise>
+            <Separator />
+        </NewInputForm>
     )
 }
+
+const FormText = styled.h5`
+  margin: 0.2rem 0 1rem 0;
+  text-shadow: 1px 1px 1px #7e7e9c;
+  letter-spacing: 0.05rem;
+  font-size: 0.8rem;
+`;
+
+const AddExercise = styled.button`
+  background: linear-gradient(to right, #2b3099 20%, #73a8e6 100%);
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  width: 37%;
+  height: 1.55rem;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
+  color: #ffe7d1;
+  font-weight: 600;
+  margin: 0.8em 0 0 0;
+`;
+
+const Separator = styled.hr`
+  width: 70%;
+  height: 0.1em;
+  border: none;
+  background-color: #73a8e6;
+  margin: 1rem 0 0.1rem 0;
+`;
 
 export default EditWorkoutForm
