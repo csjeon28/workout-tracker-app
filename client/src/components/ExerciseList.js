34d6bc4dcdcff14d31232f611c039955649753e4
Christine Jeon <csjeon28@gmail.com>
2021-09-05 20:57:44 -0400

    added scrollbar and container to styling

--- client/src/components/ExerciseList.js
+++ client/src/components/ExerciseList.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router'
 import NavBar from './NavBar'
 import Exercise from './Exercise'
 import AddExerciseForm from './AddExerciseForm'
+import styled from 'styled-components'
+import { ExerciseContainer, ScrollBar } from '../styles'
 
 const ExerciseList = ({ currentUser, setCurrentUser }) => {
+    const history = useHistory()
     const [exercises, setExercises] = useState([])
 
     useEffect(() => {
@@ -29,11 +33,31 @@ const ExerciseList = ({ currentUser, setCurrentUser }) => {
 
     return (
         <div>
-            <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser} />
-            <AddExerciseForm addExercise={addExercise} />
-            {exercisesList}
+            <ExerciseContainer>
+                <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                <AddExerciseForm addExercise={addExercise} />
+            </ExerciseContainer>
+            <BackBtn onClick={() => history.push('/userhomepage')}>Go back to Workouts</BackBtn>
+            <ScrollBar>
+                {exercisesList}
+            </ScrollBar>
         </div>
     )
 }
+
+const BackBtn = styled.button`
+  background: linear-gradient(to right, #2b3099 20%, #73a8e6 100%);
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  width: 50%;
+  height: 1.5rem;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
+  color: #ffe7d1;
+  font-weight: 600;
+  margin: 0.8em 0 0 0;
+`;
+
 
 export default ExerciseList
