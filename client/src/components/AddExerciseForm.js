bc58906b520ed93fe034004fb4fe8a4b33a38377
Christine Jeon <csjeon28@gmail.com>
2021-09-05 20:56:32 -0400

    added styling to form

--- client/src/components/AddExerciseForm.js
+++ client/src/components/AddExerciseForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+import { NewInput, AddExerciseBtn } from '../styles'
 
 const AddExerciseForm = ({ addExercise }) => {
 
@@ -15,18 +17,33 @@ const AddExerciseForm = ({ addExercise }) => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input name='name' type='text' onChange={handleChange} />
-                <label>Active Calories</label>
-                <input name='calories' type='text' onChange={handleChange} />
-                <label>Duration</label>
-                <input name='duration' type='text' onChange={handleChange} />
-                <input type='submit' value='Add Exercise' />
-            </form>
-        </div>
+        <NewExerciseForm onSubmit={handleSubmit}>
+            <FormText>Name</FormText>
+            <NewInput name='name' type='text' onChange={handleChange} />
+            <FormText>Active Calories</FormText>
+            <NewInput name='calories' type='number' onChange={handleChange} />
+            <FormText>Duration(in minutes)</FormText>
+            <NewInput name='duration' type='number' onChange={handleChange} />
+            <AddExerciseBtn type='submit' content='Add Exercise' />
+        </NewExerciseForm>
     )
 }
+
+const FormText = styled.h5`
+  margin: 0.1rem 0 0.5rem 1rem;  
+  text-shadow: 1px 1px 1px #7e7e9c;
+  letter-spacing: 0.05rem;
+  font-size: 0.8rem;
+`;
+
+const NewExerciseForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  height: 28%;
+  margin: 0.1rem 6rem 0 2rem;
+`;
+
 
 export default AddExerciseForm
