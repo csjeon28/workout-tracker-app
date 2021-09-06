09695df974311927abca4cfd013b85c2555721e9
Christine Jeon <csjeon28@gmail.com>
2021-09-05 20:54:21 -0400

    added sep container for exercise comp

---
+++ client/src/styles/ExerciseContainer.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const ExerciseContainer = styled.div`
+  display: flex;
+  align-items: left;
+  flex-direction: column;
+  height: 27vh;
+  width: 35vw;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 30px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(8.5px);
+  -webkit-backdrop-filter: blur(8.5px);
+  border-radius: 5px;
+  color: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+`;
+
+export default ExerciseContainer
