8914258fc4747e243c842f0263146d3d1f6e7be6
Christine Jeon <csjeon28@gmail.com>
2021-09-05 20:57:26 -0400

    cchanged some styling for exercises to fit page

--- client/src/components/Exercise.js
+++ client/src/components/Exercise.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-// import { Link } from 'react-router-dom'
 import EditExerciseForm from './EditExerciseForm'
+import styled from 'styled-components'
+import { EditBtn, DeleteBtn } from '../styles'
 
 const Exercise = ({ exercises, setExercises, exercise }) => {
     const [editing, setEditing] = useState(false)
@@ -16,19 +17,38 @@ const Exercise = ({ exercises, setExercises, exercise }) => {
 
     return (
         <div>
-            <div key={exercise.id}>
-                <h3>Name:{exercise.name}</h3>
-                <h3>Active Calories:{exercise.calories}</h3>
-                <h3>Duration(in minutes):{exercise.duration}</h3>
-            </div>
-            {/* <Link to={`/exercises/${exercise.id}`}>
-                <h3>{exercise.name}</h3>
-            </Link> */}
+            <ExerciseList key={exercise.id}>
+                <ExerciseText>Name:&nbsp;{exercise.name}</ExerciseText>
+                <ExerciseText>Active Calories:&nbsp;{exercise.calories}</ExerciseText>
+                <ExerciseText>Duration(in minutes):&nbsp;{exercise.duration}</ExerciseText>
+            </ExerciseList>
             {editing ? <EditExerciseForm exercises={exercises} setExercises={setExercises} exercise={exercise} editing={editing} setEditing={setEditing} />
-                : <button onClick={() => setEditing(true)}>Edit</button>}
-            <button onClick={() => deleteExercise(exercise.id)}>Delete</button>
+                : <EditBtn onClick={() => setEditing(true)} content='Edit' />}
+            <DeleteBtn onClick={() => deleteExercise(exercise.id)} content='Delete' />
         </div>
     )
 }
+
+const ExerciseText = styled.h3`
+  letter-spacing: 0.2rem;
+  text-transform: none;
+`;
+
+const ExerciseList = styled.div`
+display: flex;
+align-items: flex-end;
+flex-direction: column;
+height: 10vh;
+width: 32vw;
+background: rgba(255, 255, 255, 0.15);
+box-shadow: 0 4px 4px 0 rgba(31, 38, 135, 0.37);
+backdrop-filter: blur(12px);
+border-radius: 8px;
+color: #8a8294;
+font-size: 14px;
+text-shadow: 1px 1px 1px #ffffff;
+text-transform: uppercase;
+letter-spacing: 0.3rem;
+`;
 
 export default Exercise
