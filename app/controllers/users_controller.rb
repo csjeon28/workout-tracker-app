05ff91d3da9c528c8d5d62bf1e7a72526d74f4ae
Christine Jeon <csjeon28@gmail.com>
2021-09-08 17:51:33 -0400

    changed create to new/save so data doesnt automatically persist to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create, :index]
+    skip_before_action :authorize, only: [:create]
 
     #---------GET /users (users#index)---------
     def index
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
 
     #--------GET /users/:id (users#show)---------
     def show
-      user = User.find_by(id: session[:user_id])
+      # user = User.find_by(id: session[:user_id])
+      user = User.find_by(id: params[:id])
       if user
-        # render json: {user: user, workouts: user.workouts, exercises: user.exercises}, status: :ok
         render json: user, status: :ok
       else
         render json: {errors: ["Not Authorized"]}, status: :unauthorized
@@ -20,10 +20,11 @@ class UsersController < ApplicationController
 
     #--------POST /users (users#create)---------
     def create
-      user = User.create!(user_params)
+      # user = User.create!(user_params)
+      user = User.new(user_params)
       if user.valid?
         session[:user_id] = user.id
-      # render json: {user: user, workouts: user.workouts, exercises: user.exercises}, status: :created
+        user.save
         render json: user, status: :created
       else
         render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
