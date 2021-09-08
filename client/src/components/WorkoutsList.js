bc01c9e99e32a583dc086ee6245bcadc38d82c1c
Christine Jeon <csjeon28@gmail.com>
2021-09-08 15:13:54 -0400

    added errors to workouts list page

--- client/src/components/WorkoutsList.js
+++ client/src/components/WorkoutsList.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import Workout from './Workout'
 import AddWorkoutForm from './AddWorkoutForm'
+import Errors from './Errors'
 import { SmallContainer, ScrollBar } from '../styles'
+import { useHistory } from 'react-router-dom'
 
-const WorkoutsList = () => {
+const WorkoutsList = ({ errors, setErrors }) => {
     const [workouts, setWorkouts] = useState([])
+    const history = useHistory()
 
     useEffect(() => {
         fetch('/workouts')
             .then(resp => resp.json())
             .then(data => setWorkouts(data))
     }, [])
+
+    const handleNewWorkout = (data) => {
+        data.errors ? setErrors(data.errors) : setWorkouts([...workouts, data])
+        if (!data.errors) {
+            history.push('/workouts')
+            setErrors([])
+        }
+    }
 
     const addWorkout = (data) => {
         const config = {
@@ -22,7 +33,8 @@ const WorkoutsList = () => {
         }
         fetch('/workouts', config)
             .then(resp => resp.json())
-            .then(() => setWorkouts([...workouts, data]))
+            // .then(() => setWorkouts([...workouts, data]))
+            .then(data => handleNewWorkout(data))
     }
 
     const workoutList = workouts.map((workout, index) => <Workout key={index} workout={workout} workouts={workouts} setWorkouts={setWorkouts} />)
@@ -33,6 +45,7 @@ const WorkoutsList = () => {
                 <AddWorkoutForm addWorkout={addWorkout} />
             </SmallContainer>
             <br />
+            <Errors errors={errors} />
             <ScrollBar>
                 {workoutList}
             </ScrollBar>
